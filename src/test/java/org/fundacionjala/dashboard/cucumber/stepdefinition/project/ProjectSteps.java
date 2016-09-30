5c11ce82688fd0d1985b8f4496ea06581bc8a83c
Lourdes Villca Lazaro <LourdesVillca@Fundacion-jala.local>
2016-09-30 14:08:53 -0400

    Solve problems with project steps

--- src/test/java/org/fundacionjala/dashboard/cucumber/stepdefinition/project/ProjectSteps.java
+++ src/test/java/org/fundacionjala/dashboard/cucumber/stepdefinition/project/ProjectSteps.java
@@ -1,12 +1,11 @@
 package org.fundacionjala.dashboard.cucumber.stepdefinition.project;
 
-<<<<<<< HEAD
+
 import java.util.List;
 import java.util.Map;
 
-=======
->>>>>>> upstream/develop
 import cucumber.api.java.en.When;
+
 import org.fundacionjala.dashboard.cucumber.stepdefinition.dashboard.WidgetSteps;
 import org.fundacionjala.dashboard.ui.pages.content.ConfigTableProject;
 import org.fundacionjala.dashboard.ui.pages.content.ProjectTableWidget;
@@ -14,22 +13,20 @@ import org.fundacionjala.dashboard.ui.pages.content.Widget;
 import org.fundacionjala.dashboard.ui.pages.content.WidgetPage;
 import org.fundacionjala.dashboard.ui.pages.menu.StoryServiceForm;
 
-import java.util.Map;
-
 /**
  * Class to manage Step definition  for table widget of features.project.
  */
 public class ProjectSteps {
     private WidgetSteps widgetSteps;
 
-<<<<<<< HEAD
+
     private List<Map<String, String>> tableProjectValues;
 
-=======
+
     public ProjectSteps (WidgetSteps widgetSteps){
         this.widgetSteps = widgetSteps;
     }
->>>>>>> upstream/develop
+
     /**
      * Method to Select the features.project option form the UI.
      */
